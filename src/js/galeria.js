fa0ff96b864810978e654aae55b78892ff6bbe5d
santiagosusnik <santiagosusnik@gmail.com>
2021-11-17 14:14:07 -0300

    Initial commit

---
+++ src/js/galeria.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
+});
+
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
+
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("IMG");
+    imagen.src = `build/img/thumb/${i}.webp`;
+    imagen.dataset.imagenId = i;
+
+    imagen.onclick = mostrarImagen;
+
+    const lista = document.createElement("LI");
+    lista.appendChild(imagen);
+
+    galeria.appendChild(lista);
+  }
+}
+
+function mostrarImagen(e) {
+  const id = parseInt(e.target.dataset.imagenId);
+  //Generar la imagen
+  const imagen = document.createElement("IMG");
+  imagen.src = `build/img/grande/${id}.webp`;
+
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
+
+  overlay.onclick = function () {
+    overlay.remove();
+    body.classList.remove("fijar-body");
+  };
+
+  //boton para cerrar
+  const cerrarImagen = document.createElement("P");
+  cerrarImagen.textContent = "X";
+  cerrarImagen.classList.add("btn-cerrar");
+
+  cerrarImagen.onclick = function () {
+    overlay.remove();
+    body.classList.remove("fijar-body");
+  };
+
+  overlay.appendChild(cerrarImagen);
+
+  //mostrar en el HTML
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
+}
